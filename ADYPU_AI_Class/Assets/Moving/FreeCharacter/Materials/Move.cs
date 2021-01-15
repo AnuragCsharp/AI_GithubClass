0c58c3c5a329cd3dae61ea2b8faee3f4927bd971
AnuragCsharp <77142556+AnuragCsharp@users.noreply.github.com>
2021-01-15 14:23:14 +0530

    Upgraded to Unity 2019.4
    Just Unity Upgraded !

---
+++ ADYPU_AI_Class/Assets/Moving/FreeCharacter/Materials/Move.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour 
+{
+
+
+
+	[SerializeField]	private Vector3		goal = new Vector3(5,0,4);
+	[SerializeField]	private Vector3		InitialPosition;
+						public GameObject	Goal_Object;
+
+
+
+	void Start () 
+	{
+		InitialPosition = this.transform.position;		
+	//	this.transform.Translate(goal); //this Referers to The object where Script is attached to :) 
+	}
+	
+	void LateUpdate () 
+	{
+		this.transform.LookAt(Goal_Object.transform);
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			this.transform.Translate(goal);	
+		}
+
+		if (Input.GetKeyDown(KeyCode.B))
+		{
+			this.transform.position = InitialPosition;
+		}
+
+		if (Input.GetKey(KeyCode.S))
+		{
+			this.transform.Translate( goal * Time.deltaTime);
+		}
+
+		if (Input.GetKeyDown(KeyCode.P))
+		{
+			Vector3 position = new Vector3(0,0,0);
+			position =  Goal_Object.transform.position - this.transform.position;
+			this.transform.position = position;
+		}
+
+	}
+}
