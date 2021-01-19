17e7af88c98a8ddf3663b94a7ca4a0d222868e39
AnuragCsharp <77142556+AnuragCsharp@users.noreply.github.com>
2021-01-19 14:39:36 +0530

    Created the Move-Script
    Just to Show the Movement of a Cube and Characters from One position to Another Vector-Posiition.

---
+++ ADYPU_AI_Class/Assets/AI_Move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Move : MonoBehaviour
+{
+
+
+    public GameObject ManPlayer;
+    public GameObject DestinationGoalObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.position = DestinationGoalObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
