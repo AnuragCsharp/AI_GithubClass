52b6371f3cb85c3261b32fb1aaf02c5636dbe4c2
AnuragCsharp <77142556+AnuragCsharp@users.noreply.github.com>
2021-01-21 12:56:09 +0530

    CreatedMovingScript
    Lessons to Learn

--- ADYPU_AI_Class/Assets/AI_Move.cs
+++ ADYPU_AI_Class/Assets/AI_Move.cs
@@ -6,18 +6,18 @@ public class AI_Move : MonoBehaviour
 {
 
 
-    public GameObject ManPlayer;
-    public GameObject DestinationGoalObject;
+	public GameObject ManPlayer;
+	public GameObject DestinationGoalObject;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        this.transform.position = DestinationGoalObject.transform.position;
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		this.transform.position = DestinationGoalObject.transform.position;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
 }
