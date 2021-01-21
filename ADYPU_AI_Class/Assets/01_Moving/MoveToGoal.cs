8399b3265d074051930d990581087129522d808f
AnuragCsharp <77142556+AnuragCsharp@users.noreply.github.com>
2021-01-21 19:09:13 +0530

    Class Demo ! Ai
    Pushing the CODE to play-Around

--- ADYPU_AI_Class/Assets/01_Moving/MoveToGoal.cs
+++ ADYPU_AI_Class/Assets/01_Moving/MoveToGoal.cs
@@ -6,19 +6,46 @@ public class MoveToGoal : MonoBehaviour
 {
 
 	public	float		speed = 2.0f;
-	public	float		accuracy = 0.01f;
+	public	float		accuracy = 5;
 	public	Transform	goal;
+	public  Transform	goal2;
+
 
 	
 	void LateUpdate()
 	{
-		this.transform.LookAt(goal.position);
-		Vector3 direction = goal.position - this.transform.position; //This is the vector between the goal position and the Character. Where we Going minus where we curently are 
-		Debug.DrawRay(this.transform.position, direction, Color.red);
-	
-		if (direction.magnitude > accuracy)
+		Vector3 FirstGoalDirection = goal.position - this.transform.position; // this is our first position
+		Vector3 SecondGoalDirection = goal2.position - this.transform.position; // this is second position
+
+		if (this.gameObject.name == "Man")
 		{
-			this.transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+			Debug.DrawRay(this.transform.position, FirstGoalDirection, Color.red);
+			this.transform.LookAt(goal.position);
+
+			if (Input.GetKey(KeyCode.Space))
+			{
+				if (FirstGoalDirection.magnitude > accuracy)
+				{
+					this.transform.Translate(FirstGoalDirection.normalized * speed * Time.deltaTime, Space.World);
+				}
+			}
+
 		}
+		else
+		{
+			Debug.DrawRay(this.transform.position, SecondGoalDirection, Color.green);
+			this.transform.LookAt(goal2.position);
+
+			if (Input.GetKey(KeyCode.P))
+			{
+				if (SecondGoalDirection.magnitude > accuracy)
+				{
+					this.transform.Translate(SecondGoalDirection.normalized * speed * Time.deltaTime, Space.World);
+				}
+			}
+		}
+
+
+
 	}
 }
